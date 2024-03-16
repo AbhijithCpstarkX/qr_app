6ff46a77e52822e399c09e379c4ccc6a6055c289
AbhijithCpstarkX <160301332+AbhijithCpstarkX@users.noreply.github.com>
2024-03-16 15:22:51 +0530

    ygg

--- lib/registration.dart
+++ lib/registration.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
+import 'package:loginpage/loginscreen.dart';
 import 'package:loginpage/qrpage.dart';
 
 class Reg extends StatefulWidget {
@@ -9,6 +12,33 @@ class Reg extends StatefulWidget {
 }
 
 class _RegState extends State<Reg> {
+  TextEditingController name=TextEditingController();
+  TextEditingController rollno=TextEditingController();
+  TextEditingController email=TextEditingController();
+  TextEditingController pass=TextEditingController();
+
+  void register()async {
+   Uri uri = Uri.parse('https://scnner-web.onrender.com/api/register');
+   var response = await http.post(uri,
+     headers:<String, String>{
+     'Content-Type' : 'application/json; charset=UTF-8'
+   },
+     body: jsonEncode({
+       'name': name.text,
+       'email': email.text,
+       'rollno': rollno.text,
+       'password': pass.text,
+     })
+   );
+   print(response.statusCode);
+   print(response.body);
+   if(response.statusCode == 200) {
+     Navigator.push(context, MaterialPageRoute(builder: (context)=>const LoginScreen()));
+   }else{
+     // ScaffoldMessenger.of(context).showSnackBar(SnackBar(content:Text('SOMETHING WENT WRONG')));
+   }
+
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -23,7 +53,9 @@ class _RegState extends State<Reg> {
                 SizedBox(height: 30,),
                 Padding(
                   padding: const EdgeInsets.all(20),
-                  child: TextField(style: TextStyle(color: Colors.white),
+                  child: TextField(
+                    controller: name,
+                    style: TextStyle(color: Colors.white),
                     decoration: InputDecoration(
                       enabledBorder: OutlineInputBorder(borderSide:BorderSide(color: Colors.white),borderRadius: BorderRadius.all(Radius.circular(10))),
                       hintText: 'Enter Your Name',
@@ -32,7 +64,9 @@ class _RegState extends State<Reg> {
                 ),
                 Padding(
                   padding: const EdgeInsets.all(20),
-                  child: TextField(style: TextStyle(color: Colors.white),
+                  child: TextField(
+                    controller: rollno,
+                    style: TextStyle(color: Colors.white),
                     decoration: InputDecoration(
                       enabledBorder:
                       OutlineInputBorder(
@@ -46,7 +80,9 @@ class _RegState extends State<Reg> {
 
                 Padding(
                   padding: const EdgeInsets.all(20),
-                  child: TextField(style: TextStyle(color: Colors.white),
+                  child: TextField(
+                    controller: email,
+                    style: TextStyle(color: Colors.white),
                     decoration: InputDecoration(
                       enabledBorder:
                       OutlineInputBorder(
@@ -60,7 +96,9 @@ class _RegState extends State<Reg> {
 
                 Padding(
                   padding: const EdgeInsets.all(20),
-                  child: TextField(style: TextStyle(color: Colors.white),
+                  child: TextField(
+                    controller: pass,
+                    style: TextStyle(color: Colors.white),
                     decoration: InputDecoration(
                       enabledBorder:
                       OutlineInputBorder(
@@ -74,10 +112,12 @@ class _RegState extends State<Reg> {
                 ElevatedButton(
 
                   child: Text('Register',style: TextStyle(color: Colors.white,fontSize: 20)),
-                  onPressed: () {Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => const QrPage()),
-                  );
+                  onPressed: () {register();
+                    //print(name.text);
+                    //print(rollno.text);
+                    //print(email.text);
+                    //print(pass.text);
+                    //Navigator.push(context, MaterialPageRoute(builder: (context) => const QrPage()),);
                   },
                   style: ElevatedButton.styleFrom(
                       backgroundColor: Colors.teal,
